d92d5478b119752de100cf8e8ea9e18d955e95fc
Ji, Heejoon <kongla3214@gmail.com>
2025-10-20 15:48:58 +0900

    five

--- Main.java
+++ Main.java
@@ -1,5 +1,7 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("지희준");
-    }
+
+  public static void main(String[] args) {
+    System.out.println("홍길동");
+    Hello.run();
+  }
 }
